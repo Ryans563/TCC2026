b77fbbb9f03aed37ed558993d7c4789b360aec3b
logsilva <logsilva44@gmail.com>
2026-05-14 14:18:10 -0300

    Update home.php

--- TCC_RELPJAM/app/views/home.php
+++ TCC_RELPJAM/app/views/home.php
@@ -111,7 +111,7 @@ $base = "/TCC_RELPJAM";
             (currentIndex * totalCardWidth);
     
         track.style.transform =
-            `translateX(${centerPosition - moveX + 30}px)`;
+            `translateX(${centerPosition - moveX + 20}px)`;
     }
     
     function moveCarousel(direction) {
