772ed0cd8a44d25d8860c4d4fdc0c28b6b584302
logsilva <logsilva44@gmail.com>
2026-06-01 11:17:32 -0600

    Update add_produto.php

--- TCC_RELPJAM/app/views/add_produto.php
+++ TCC_RELPJAM/app/views/add_produto.php
@@ -126,7 +126,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Adicionar Produto</title>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="<?= $base ?>/public/assets/css/style.css">
 
 </head>
 <body>
