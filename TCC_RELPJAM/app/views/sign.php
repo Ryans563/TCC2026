276dce0fafa73e875e6a9194d033946cb61157a7
Ryans563 <r1294869@gmail.com>
2026-06-09 13:29:30 -0300

    Update stylesheet link for sign.php

--- TCC_RELPJAM/app/views/sign.php
+++ TCC_RELPJAM/app/views/sign.php
@@ -158,7 +158,7 @@ if (isset($_POST['excluir'])) {
 
     <link
         rel="stylesheet"
-        href="css/cadastro.css">
+        href="public/assets/css/stylecad.css">
 
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
