b8f64f0a9e3d32daed8b2fc649874a843c4b96d6
Ryans563 <r1294869@gmail.com>
2026-06-09 13:32:05 -0300

    Update CSS link for vendor registration page

--- TCC_RELPJAM/app/views/vendedorcad.php
+++ TCC_RELPJAM/app/views/vendedorcad.php
@@ -90,7 +90,7 @@ content="width=device-width, initial-scale=1.0">
 
 <link
 rel="stylesheet"
-href="css/vendedor.css">
+href="public/assets/css/stylevend.css">
 
 <link
 href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
