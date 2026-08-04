1503476a0491264bcab446d8cbc8344a7868880e
Jao-lab51 <soaresjoaomiguel258@gmail.com>
2026-08-03 22:27:54 -0300

    caminho das pastas sendo verificado

--- TCC_RELPJAM/app/views/carrinho.php
+++ TCC_RELPJAM/app/views/carrinho.php
@@ -35,7 +35,7 @@ foreach ($cartProdutos as $p) {
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link rel="stylesheet" href="../../public/assets/css/carrinho.css">
+    <link rel="stylesheet" href="../../public/assets/css/style.css">
 </head>
 <body class="carrinho-body">
 <div class="carrinho-container">
